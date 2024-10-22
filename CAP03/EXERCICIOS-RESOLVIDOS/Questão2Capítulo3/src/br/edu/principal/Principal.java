8366b6981e388d502dcbb45787e7344a24168256
Willame Silva <willame.silva@aluno.ifce.edu.br>
2024-10-22 15:26:06 -0300

    Adicionando a Atividade 2

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Questão2Capítulo3/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double n1, n2, n3, n4;
+		n1 = 5.0;
+		n2 = 5.0;
+		n3 = 5.0;
+		n4 = 5.0;
+		double media = (n1+n2+n3+n4)/4;
+		System.out.println("A média das notas é: " + media);
+	}
+
+}
---
+++ CAP03/EXERCICIOS-RESOLVIDOS/Questão2Capítulo3/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Questão2Capítulo3 {
+}
